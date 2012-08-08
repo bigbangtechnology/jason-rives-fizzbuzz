562fd71c38a5dba2660a9fb962213e23270253ed
Cameron Westland <camwest@gmail.com>
2012-08-08 09:40:45 -0400

    Adding basic rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasonRivesFizzbuzz::Application.config.secret_token = '1a9fd1d8e88046197a9d5587835e3d31bbce92eda6ceafcb46acaf8fb9416881a0669fc61a21e411533d4c94dc5745ec1c50f61ef8b35c5b1af55e41d0ebcd41'
